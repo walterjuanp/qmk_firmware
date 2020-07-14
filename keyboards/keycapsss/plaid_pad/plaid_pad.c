62e194d73e435b8a766a7ccc5f4c9fa4c4421d57
BenRoe <mail@benjaminroesner.com>
2020-07-14 17:59:30 +0200

    set led pins as output
    Without that the pinvoltage is only 1.8V. This couses not bright led’s.
    If the pins are set as output, they deliver 4.8V.

--- keyboards/keycapsss/plaid_pad/plaid_pad.c
+++ keyboards/keycapsss/plaid_pad/plaid_pad.c
@@ -1,1 +1,8 @@
 #include "plaid_pad.h"
+
+void matrix_init_user(void) {
+    // comment out this lines to reduce the led brightness
+    // with setPinOutput pin has 4.8V and without 1.8V
+    setPinOutput(C5); //Led1
+    setPinOutput(C4); //Led2
+}
